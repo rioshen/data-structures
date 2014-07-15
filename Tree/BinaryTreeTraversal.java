7db94267d949062490525b1824927fcb15a70859
rioshen <rioxshen@gmail.com>
2014-07-14 21:03:48 -0400

    update in order and post order traversal

--- Tree/BinaryTreeTraversal.java
+++ Tree/BinaryTreeTraversal.java
@@ -19,9 +19,41 @@ public class BinaryTreeTraversal {
         stack.push(root);
         while (!stack.isEmpty()) {
             TreeNode node = stack.pop();
-            System.out.print(node.value);
+            System.out.print(node.value + " ");
             if (node.right != null) stack.push(node.right);
             if (node.left != null) stack.push(node.left);
+        }
+    }
+
+    public static void postorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            System.out.print(node.value + " ");
+            if (node.left != null) stack.push(node.left);
+            if (node.right != null) stack.push(node.right);
+        }
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null) return;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                System.out.print(node.value + " ");
+                node = node.right;
+            }
         }
     }
 
@@ -30,8 +62,8 @@ public class BinaryTreeTraversal {
          *    10
          *  /   \
          * 8      2
-         */  \    /
-         *3     5  2
+         * /\    /
+         *3  5  2
          */
         TreeNode root = new TreeNode(10);
         root.left        = new TreeNode(8);
@@ -40,5 +72,9 @@ public class BinaryTreeTraversal {
         root.left.right = new TreeNode(5);
         root.right.left = new TreeNode(2);
         preorder(root);
+        System.out.println();
+        postorder(root);
+        System.out.println();
+        inorder(root);
     }
 }
